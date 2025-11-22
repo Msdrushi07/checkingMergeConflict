b133c89b41b1af464cb93d3b4ebeece0653ea444
rushikesh <manerushikesh40@gmail.com>
2025-11-22 19:54:54 +0530

    edited feature6 file

--- src/com/java1/Feature1.java
+++ src/com/java1/Feature1.java
@@ -2,6 +2,6 @@ package com.java1;
 
 public class Feature1 {
     public static void main(String[] args) {
-        System.out.println("now crating merge conflict");
+        System.out.println("added feature6 file");
     }
 }
