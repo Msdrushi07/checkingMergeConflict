eba5879559b6d7fcb836f9a0e07b4c0e1861b785
rushikesh <manerushikesh40@gmail.com>
2025-11-22 20:01:37 +0530

    edited file which is edited by 6

--- src/com/java1/Feature1.java
+++ src/com/java1/Feature1.java
@@ -3,5 +3,8 @@ package com.java1;
 public class Feature1 {
     public static void main(String[] args) {
         System.out.println("now crating merge conflict");
+
+
+        System.out.println("editing file which is done by feature 6 but different line");
     }
 }
