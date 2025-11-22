1bb4b9becfbedf369a49b454e4e3146e4e65fca3
rushikesh <manerushikesh40@gmail.com>
2025-11-22 19:35:38 +0530

    edited feature1 file

--- src/com/java1/Feature1.java
+++ src/com/java1/Feature1.java
@@ -2,6 +2,6 @@ package com.java1;
 
 public class Feature1 {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        System.out.println("edited featire1 file!");
     }
 }
