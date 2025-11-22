fea6ca639b82279ec3e5f53de549992b0821dcca
Msdrushi07 <108717120+Msdrushi07@users.noreply.github.com>
2025-11-22 20:27:54 +0530

    Add manual edit message to Feature1.java

--- src/com/java1/Feature1.java
+++ src/com/java1/Feature1.java
@@ -3,5 +3,6 @@ package com.java1;
 public class Feature1 {
     public static void main(String[] args) {
         System.out.println("added feature6 file");
+         System.out.println("manually edited");
     }
 }
