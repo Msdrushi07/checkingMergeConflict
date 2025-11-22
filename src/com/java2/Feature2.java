a9c85c256365e7fae8d69854fe53ceefc730299c
rushikesh <manerushikesh40@gmail.com>
2025-11-22 19:56:51 +0530

    added feature7 file

--- src/com/java2/Feature2.java
+++ src/com/java2/Feature2.java
@@ -2,6 +2,6 @@ package com.java2;
 
 public class Feature2 {
     public static void main(String[] args) {
-        System.out.println("editing feature 2 file!");
+        System.out.println("added feature 7!");
     }
 }
