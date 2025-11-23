d5d27418a27ecd8fec514edb51796b38fec0e54b
Msdrushi07 <108717120+Msdrushi07@users.noreply.github.com>
2025-11-23 18:18:03 +0530

    Add duplicate merge conflict message to Feature1

--- src/com/java1/Feature1.java
+++ src/com/java1/Feature1.java
@@ -4,6 +4,7 @@ public class Feature1 {
     public static void main(String[] args) {
 
         System.out.println("now crating merge conflict");
+        System.out.println("now crating merge conflict");
         
 
 
