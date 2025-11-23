00d83b765cbc394fb9685dfc5510fb9da707ad18
Msdrushi07 <108717120+Msdrushi07@users.noreply.github.com>
2025-11-23 18:05:18 +0530

    Update Feature3.java

--- src/com/java3/Feature3.java
+++ src/com/java3/Feature3.java
@@ -4,6 +4,7 @@ public class Feature3 {
     static void main(String[] args) {
 
         System.out.println("created feature 3 file");
+        System.out.println("again creating for better understanding");
         System.out.println("creating conflict");
          System.out.println("added new line");
         System.out.println("again creating for better understanding");
