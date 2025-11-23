996acec2805a9b470bfcfc27b84d00e380052645
Msdrushi07 <108717120+Msdrushi07@users.noreply.github.com>
2025-11-23 18:22:42 +0530

    Add print statement for feature 7

--- src/com/java2/Feature2.java
+++ src/com/java2/Feature2.java
@@ -2,7 +2,7 @@ package com.java2;
 
 public class Feature2 {
     public static void main(String[] args) {
-
+ System.out.println("added feature 7!");
         System.out.println("added feature 7!");
     }
 }
